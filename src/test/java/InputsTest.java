d92347d3700f7c4ce9bbbbb40781da0204b1bb80
Anna Krasnokutskaia <malepops@gmail.com>
2026-04-15 20:54:27 +0300

    дз 1 по селениуму сделано

---
+++ src/test/java/InputsTest.java
@@ -0,0 +1,83 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.time.Duration;
+
+/*Проверить на возможность ввести различные цифровые и
+нецифровые значения, используя Keys.ARROW_UP И
+Keys.ARROW_DOWN */
+
+public class InputsTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        //задаем опции для нашего драйвера
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+
+        //определяем браузер с которым хотим работать
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void checkInputs() {
+
+        SoftAssert softAssert = new SoftAssert();
+
+        //неявное ожидание
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //открывает страницу по указанному url
+        driver.get("https://the-internet.herokuapp.com/inputs");
+
+        WebElement input = driver.findElement(By.tagName("input"));
+
+        input.click();
+
+        //ввести буквы
+        String letters = "abcd";
+        input.sendKeys(letters);
+        softAssert.assertEquals(input.getAttribute("value"),"");
+        input.clear();
+
+        //ввести спецсимволы
+        String specialCharacters = "&?!!!?///";
+        input.sendKeys(specialCharacters);
+        softAssert.assertEquals(input.getAttribute("value"),"");
+        input.clear();
+
+        //ввести цифры
+        String numbers = "10";
+        input.sendKeys(numbers);
+        softAssert.assertEquals(input.getAttribute("value"), numbers);
+
+        //пощёлкать вверх
+        int num = Integer.parseInt(numbers);
+        input.sendKeys(Keys.ARROW_UP);
+        softAssert.assertEquals(input.getAttribute("value"),String.valueOf(num+1));
+
+        //пощёлкать вниз
+        input.sendKeys(Keys.ARROW_DOWN);
+        softAssert.assertEquals(input.getAttribute("value"),numbers);
+
+        //проверка всего
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/TyposTest.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.time.Duration;
+
+/*Проверить соответствие параграфа орфографии (на уроке сказали повторить 10 раз)*/
+
+public class TyposTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        //задаем опции для нашего драйвера
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+
+        //определяем браузер с которым хотим работать
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void checkTypos(){
+
+        SoftAssert softAssert = new SoftAssert();
+
+        //неявное ожидание
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //открывает страницу по указанному url
+        driver.get("https://the-internet.herokuapp.com/typos");
+
+        for (int i = 0; i < 10; i++){
+            //refresh page
+            driver.navigate().refresh();
+            //получить текст
+            String text = driver.findElement(By.xpath("(//p)[2]")).getText();
+            //проверка одной штуки
+            softAssert.assertEquals(text,"Sometimes you'll see a typo, other times you won't.");
+        }
+
+        //проверка всего
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
