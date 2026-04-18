01cb03054f9e3e9d6564f1d27e39f84e12d19588
Anna Krasnokutskaia <malepops@gmail.com>
2026-04-18 12:42:57 +0300

    дз 1 по селениуму, задачи для выходных

---
+++ src/test/java/DropdownTest.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.time.Duration;
+import java.util.List;
+
+/*Взять все элементы дроп-дауна и проверить их наличие.
+Выбрать первый, проверить, что он выбран, выбрать второй, проверить, что
+он выбран*/
+
+public class DropdownTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        //задаем опции для нашего драйвера
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+
+        //определяем браузер с которым хотим работать
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void checkDropdown() {
+
+        SoftAssert softAssert = new SoftAssert();
+
+        //неявное ожидание
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //открывает страницу по указанному url
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+        //получим наш список
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        //проверить содержание
+        List<WebElement> options = select.getOptions();
+        softAssert.assertEquals(options.size(), 3);
+        softAssert.assertEquals(options.get(0).getText(), "Please select an option");
+        softAssert.assertEquals(options.get(1).getText(), "Option 1");
+        softAssert.assertEquals(options.get(2).getText(), "Option 2");
+
+        //выбрать первый
+        select.selectByValue("1");
+        softAssert.assertTrue(options.get(1).isSelected());
+
+        //выбрать второй
+        select.selectByValue("2");
+        softAssert.assertTrue(options.get(2).isSelected());
+
+        //проверка всего
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
